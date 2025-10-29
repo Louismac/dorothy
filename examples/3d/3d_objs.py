4d451df738167dbd058209f82e51f0bcbc52ab03
Louis Busby <l.mccallum@arts.ac.uk>
2025-10-29 15:45:16 +0000

    obj exampl

--- examples/3d/3d_objs.py
+++ examples/3d/3d_objs.py
@@ -8,9 +8,10 @@ class Example3D:
 
     def setup(self):
         dot.camera_3d()
-        dot.set_camera((20, 20, 20), (0, 0, 0))
+        dot.set_camera((40, 40, 40), (0, 0, 0))
         # Load mesh
-        self.tree = dot.load_obj("model/Tree1.obj")
+        self.model = dot.load_obj("model/cottage_obj.obj")
+        #self.model = dot.load_obj("model/Tree1.obj")
         self.texture_layer = dot.get_layer()
 
     def draw(self):
@@ -18,7 +19,8 @@ class Example3D:
         # Draw dynamic texture
         with dot.layer(self.texture_layer):
             dot.camera_2d()
-            dot.background((50, 50, 100,10))
+            dot.fill((255, 0, 0, 100))
+            dot.rectangle((0,0),(dot.width, dot.height-2))
             dot.fill((255, 200, 100))
             x = (dot.frames * 5) % dot.width
             dot.circle((x, dot.height//2), 50)
@@ -26,8 +28,14 @@ class Example3D:
         # Draw mesh
         with dot.transform():
             dot.camera_3d()
-            dot.rotate(dot.frames * 0.01, 0, 1, 0)
-            dot.fill((200, 150, 100))
-            dot.draw_mesh(self.tree, self.texture_layer)
+            dot.rotate(dot.frames * 0.005, 0, 1, 0)
+            # fill with colour
+            dot.fill((0,0,255,50))
+            dot.draw_mesh(self.model)
+            #or texture with a layer
+            dot.translate(0,20,0)
+            dot.draw_mesh(self.model, self.texture_layer)
+
+            
 
 Example3D()
