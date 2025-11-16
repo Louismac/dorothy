a8b6e9348315c34dff182b5ebd09c377b79126d5
Louis Busby <l.mcccallum@arts.ac.uk>
2025-11-16 22:19:26 +0000

    moving grid

--- examples/3d/3d_lines_lots.py
+++ examples/3d/3d_lines_lots.py
@@ -10,7 +10,7 @@ class Example3D:
     def setup(self):
         print("3D Setup!")
         dot.camera_3d()
-        self.camera_pos = -45
+        self.camera_pos = (3,3,-45)
         self.s = 10  # spacing between cubes
         self.cube_size = 7  # grid dimensions
         self.z = 0
@@ -19,7 +19,7 @@ class Example3D:
         self.angle_x = 0
         self.dir_y = 0.02
         self.angle_y = 0
-        dot.set_camera((1, -2, self.camera_pos), (0, 0, 0))
+        dot.set_camera(self.camera_pos, (0, 0, 0))
 
     def draw_cube(self, size, offset=(0, 0, 0)):
         s = size / 2
@@ -56,7 +56,7 @@ class Example3D:
             self.dir_x = -self.dir_x
         x = 8 * np.cos(self.angle_x)
         y = 6 * np.sin(self.angle_y)
-        dot.set_camera((1, -2, self.camera_pos),(x, y, 0))
+        dot.set_camera(self.camera_pos,(x, y, 0))
         
         # Wrap z when it exceeds one cube spacing
         if self.z <= self.s*4:
@@ -74,9 +74,7 @@ class Example3D:
                         x = (i * self.s) - offset
                         y = (j * self.s) - offset
                         z = (k * self.s) - offset - self.z
-                        
-                        # Wrap z coordinate to create infinite tunnel
-                        # Keep cubes within visible range
+    
                         total_depth = self.s * self.cube_size
                         while z < -total_depth / 2:
                             z += total_depth
